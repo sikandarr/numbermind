a3980f4b16aaeb3dd2362d4b11a25674e4da2376
Sikandar <sikandar.b@mail.com>
2017-10-24 19:06:11 +1030

    runtime now less than a second for sample input

--- src/solution/genetic/Solution.java
+++ src/solution/genetic/Solution.java
@@ -5,7 +5,10 @@ import java.util.*;
 
 public class Solution
 {
-	static Random random = new Random();
+	//static long seed = new Random().nextLong();
+	static long seed = 2001923711459688263L;
+	static Random random = new Random(seed);
+	static ArrayList<IncorrectDigit> incorrectDigits = new ArrayList<IncorrectDigit>();
 
 	static class Guess
 	{
@@ -35,6 +38,67 @@ public class Solution
 		{
 			return candidate + " " + fitness;
 		}
+	}
+
+	public static class IncorrectDigit
+	{
+		char digit;
+		ArrayList<Integer> pos;
+
+		IncorrectDigit(char digit)
+		{
+			this.digit = digit;
+			pos = new ArrayList<Integer>();
+		}
+
+		public void initialize(char[] incorrectGuess)
+		{
+			for (int i = 0; i < incorrectGuess.length; i++)
+			{
+				if (digit == incorrectGuess[i])
+					pos.add(i);
+			}
+		}
+
+		public static ArrayList<IncorrectDigit> construct(String incorrectGuess)
+		{
+			ArrayList<IncorrectDigit> incorrectDigits = new ArrayList<IncorrectDigit>();
+			char[] guess = incorrectGuess.toCharArray();
+			for (int i = 0; i < guess.length; i++)
+			{
+				IncorrectDigit incorrectDigit = new IncorrectDigit(guess[i]);
+				incorrectDigit.initialize(guess);
+				incorrectDigits.add(incorrectDigit);
+			}
+			return incorrectDigits;
+		}
+	}
+
+	public static boolean isCorrect(char c, int pos)
+	{
+		for (IncorrectDigit id : incorrectDigits)
+		{
+			if (id.digit == c)
+			{
+				for (int i : id.pos)
+					if (i == pos)
+						return false;
+			}
+		}
+		return true;
+	}
+
+	public static String replaceAllIncorrect(String candidate)
+	{
+		char[] target = candidate.toCharArray();
+		for (int i = 0; i < target.length; i++)
+		{
+			for (; !isCorrect(target[i], i);)
+			{
+				target[i] = (char) (random.nextInt(10) + 48);
+			}
+		}
+		return String.valueOf(target);
 	}
 
 	public static int fitness(String candidate, Guess[] guesses)
@@ -70,16 +134,18 @@ public class Solution
 				offspring[i] = parentA[i];
 			else offspring[i] = parentB[i];
 		}
-
+		
 		int[] mutations = new int[mutate];
 
 		for (int i = 0; i < mutations.length; i++)
 			mutations[i] = random.nextInt(parentA.length);
 
 		for (int i = 0; i < mutations.length; i++)
+		{
 			offspring[mutations[i]] = (char) (random.nextInt(10) + 48);
+		}
 
-		return String.valueOf(offspring);
+		return replaceAllIncorrect(String.valueOf(offspring));
 	}
 
 	public static void main(String[] args) throws FileNotFoundException
@@ -94,13 +160,17 @@ public class Solution
 			String line = scan.nextLine();
 			String s[] = line.split(" ");
 			guesses[i] = new Guess(s[0], new Integer(s[1]));
+			if (guesses[i].correctDigits == 0)
+				incorrectDigits.addAll(IncorrectDigit.construct(guesses[i].guess));
 		}
+
 		Candidate winner = null;
 		ArrayList<Candidate> population = new ArrayList<Candidate>();
+		final long startTime = System.currentTimeMillis();
 		for (;;)
 		{
-			Candidate candidate = new Candidate(generate(guesses[0].guess.length()), guesses);
-			if (candidate.fitness > 0)
+			Candidate candidate = new Candidate(replaceAllIncorrect(generate(guesses[0].guess.length())), guesses);
+			if (candidate.fitness > (guesses.length / 4))
 				population.add(candidate);
 
 			if (candidate.fitness == guesses.length)
@@ -109,7 +179,7 @@ public class Solution
 				break;
 			}
 
-			if (candidate.fitness >= (int) (guesses.length / 1.2))
+			if (candidate.fitness >= guesses.length / 2.5)
 				break;
 		}
 		if (winner == null)
@@ -117,12 +187,12 @@ public class Solution
 			{
 				Candidate parentA = population.get(random.nextInt(population.size()));
 				Candidate parentB = population.get(population.size() - 1);
-				String offspring = mutate(parentA.candidate.toCharArray(), parentB.candidate.toCharArray(), 2);
+				String offspring = mutate(parentA.candidate.toCharArray(), parentB.candidate.toCharArray(), (int) (guesses.length/2.8));
 				Candidate candidate = new Candidate(offspring, guesses);
 
-				if (candidate.fitness >= parentB.fitness)
+				if (candidate.fitness >= parentB.fitness && !candidate.candidate.equals(parentB.candidate))
 				{
-					System.out.println(candidate);
+					//System.out.println(candidate);
 					population.add(candidate);
 				}
 
@@ -133,6 +203,10 @@ public class Solution
 				}
 
 			}
+		final long endTime = System.currentTimeMillis();
+		System.out.println();
+		System.out.println("Total execution time: " + (endTime - startTime));
+		//System.out.println("Seed: " + seed + "ArrayList Size: " + population.size());
 		System.out.println(winner.candidate);
 
 	}
